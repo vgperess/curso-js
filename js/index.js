d85f3e111603a6427eba61e2cd0a4c38da452b42
Victor Garcia <vgperess1@gmail.com>
2026-01-31 13:23:34 -0300

    3a aula (declarações)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-document.getElementById("text").innerHTML = "Texto alterado pelo JavaScript! <b> JS </b>";
-alert("Oi, isso é um alerta!!");
-console.log("Olá, isso é um log no console!");
+//document.getElementById("text").innerHTML = "Texto alterado pelo JavaScript! <b> JS </b>";
+//alert("Oi, isso é um alerta!!");
+//console.log("Olá, isso é um log no console!");
+
+
+// Declaração de variáveis
+let a,b,c;
+a = 10;
+b = 20;
+c = 30;
+
+let pessoa = "Victor";
+let pessoa2='Vito'; // espaços não importam, mas são interessantes.
+
+document.getElementById("text").innerHTML = 
+"Texto alterado pelo JavaScript! <b> JS </b>"; //quebra de linhas são aceitas, mas manter tudo na mesma linha é mais comum.
+
+function minhaFuncao(){
+    let x = 100;
+    let y = 200;
+    let z = x + y;
+    console.log("O valor de z é: " + z);
+}
