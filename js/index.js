eb12fe9798e07df6b43e42b229adff3bc5fc0dab
Victor Garcia <vgperess1@gmail.com>
2026-01-31 13:14:44 -0300

    2a aula

--- js/index.js
+++ js/index.js
@@ -1,1 +1,3 @@
-alert("Olá, mundo! de outro arquivo");
+document.getElementById("text").innerHTML = "Texto alterado pelo JavaScript! <b> JS </b>";
+alert("Oi, isso é um alerta!!");
+console.log("Olá, isso é um log no console!");
