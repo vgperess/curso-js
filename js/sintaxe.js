c848f8fa34cf25a5d125f11feb4e9eb891b7f4b9
Victor Garcia <vgperess1@gmail.com>
2026-01-31 13:31:30 -0300

    4a aula (sintaxe)

---
+++ js/sintaxe.js
@@ -0,0 +1,14 @@
+console.log( 5 + 3 ); //ele faz a operação e mostra o resultado no console
+console.log("10 - 2"); //ele apenas concatena os valores como texto
+
+//Tipos de dados
+var a = 1;
+var b = 5;
+var numero = a + b; //número
+var mult = a * b; //número
+
+console.log(numero);
+console.log(mult);
+
+console.log(texto);
+console.log(booleano);
